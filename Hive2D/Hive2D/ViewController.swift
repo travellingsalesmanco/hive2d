ac09f416158c15f41e4d1967e515d504fc478bbb
Adam Chew <adamwth@hotmail.com>
2020-03-14 14:25:50 +0800

    Init code base

---
+++ Hive2D/Hive2D/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hive2D
+//
+//  Created by Adam Chew Yong Soon on 14/3/20.
+//  Copyright © 2020 TSCO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
