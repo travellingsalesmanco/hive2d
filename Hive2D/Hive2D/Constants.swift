d18667dbde51dd0dcd82d2960fbaa52279777547
Yu Jia Tay <yujiatay@gmail.com>
2020-03-17 22:52:07 +0800

    Add constants

---
+++ Hive2D/Hive2D/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  Hive2D
+//
+//  Created by Yu Jia Tay on 17/3/20.
+//  Copyright © 2020 TSCO. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    struct StoryBoardIds {
+        static let chooseNameModal = "ChooseNameModal"
+        static let joinGameModal = "JoinGameModal"
+        static let lobbyViewController = "LobbyViewController"
+    }
+
+    struct LobbyMessages {
+        static let fontSize = UIFont.systemFont(ofSize: 30)
+        static let createLobby = "Creating game lobby..."
+        static let joinLobby = "Joining game lobby..."
+        static let noPlayer = "Nobody :("
+    }
+}
