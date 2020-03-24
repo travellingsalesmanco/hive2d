393a81b853bac429fcb57e6c572669dc0e0bd32f
Adam Chew <adamwth@hotmail.com>
2020-03-24 12:51:40 +0800

    Add node link

--- Hive2D/Hive2D/Game/Game.swift
+++ Hive2D/Hive2D/Game/Game.swift
@@ -130,6 +130,37 @@ class Game {
             return true
         }
     }
+
+
+    func connectNodeToNearest(from: NodeComponent) {
+        let nodes = query(includes: NodeComponent.self)
+            .map { $0.component(ofType: NodeComponent.self) }
+        var closestNode: NodeComponent?
+        var closestDistance = CGFloat.infinity
+        nodes.forEach { node in
+            guard node != nil && node != from else {
+                return
+            }
+            let distanceX = pow(node!.position.x - from.position.x, 2)
+            let distanceY = pow(node!.position.y - from.position.y, 2)
+            let distanceSquared = distanceX + distanceY
+            if distanceSquared < closestDistance {
+                closestNode = node
+                closestDistance = distanceSquared
+            }
+        }
+        connectNode(from: from, to: closestNode!)
+    }
+
+    func connectNode(from: NodeComponent, to: NodeComponent) {
+        let path = CGMutablePath()
+        path.move(to: from.position)
+        path.addLine(to: to.position)
+        let edge = SKShapeNode(path: path)
+        edge.strokeColor = .red
+        scene.addChild(edge)
+    }
+
     private func handleGameActionsInQueue() {
         let actions = gameNetworking.gameActionQueue.dequeueAll()
         for action in actions {
@@ -210,6 +241,7 @@ class Game {
             return
         }
         add(entity: resourceNode)
+        connectNodeToNearest(from: nodeComponent)
     }
     func handleDestroyNode(_ action: DestroyNodeAction) {
         guard let node = networkedEntities[action.nodeNetId] else {
