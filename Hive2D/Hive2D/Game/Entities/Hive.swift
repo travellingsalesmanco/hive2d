a01a3084234c1ecba9b7cd95725ded6b82bfbf68
Foo Guo Wei <fooguowei3@gmail.com>
2020-04-26 15:32:52 +0800

    Refactor Hive to inherit from Node

--- Hive2D/Hive2D/Game/Entities/Hive.swift
+++ Hive2D/Hive2D/Game/Entities/Hive.swift
@@ -6,27 +6,5 @@
 //  Copyright © 2020 TSCO. All rights reserved.
 //
 
-import GameplayKit
-
-class Hive: GameEntity {
-
-    init(sprite: SpriteComponent,
-         minimapDisplay: MinimapComponent,
-         node: NodeComponent,
-         transform: TransformComponent,
-         player: PlayerComponent,
-         network: NetworkComponent) {
-        super.init()
-        addComponent(transform)
-        addComponent(sprite)
-        addComponent(minimapDisplay)
-        addComponent(node)
-        addComponent(player)
-        addComponent(network)
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+class Hive: Node {
 }
